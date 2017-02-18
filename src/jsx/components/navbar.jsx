6531a11256321c0d12c9cc2afdebfcb79d60e174
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-02-18 19:39:13 +0100

    Add navbar styles and improve list rendering

--- src/jsx/components/navbar.jsx
+++ src/jsx/components/navbar.jsx
@@ -2,15 +2,37 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class Navbar extends React.Component {
+
+  /**
+   * @param {Object} props
+   */
+  constructor(props) {
+    super(props);
+
+    this.links = [
+      { title: 'Home', link: '/' },
+      { title: 'Keys', link: '/keys' },
+      { title: 'About', link: '/about' }
+    ];
+  }
+
   render() {
+    let listItems = this.links.map((item) => {
+      return (
+        <li key={item.title} className="navbar__list-item">
+          <Link className="navbar__link" to={item.link}>{item.title}</Link>
+        </li>
+      );
+    });
+
     return (
-      <div className="navbar">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/keys">Keys</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
+      <div className="container-fluid navbar">
+        <div className="container">
+          <ul>
+            {listItems}
+          </ul>
+        </div>
       </div>
-    )
+    );
   };
 }
