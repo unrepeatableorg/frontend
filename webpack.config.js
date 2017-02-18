af7634e77cc45056d7f131f2d28b4cd1cc564bb5
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-02-18 16:08:08 +0100

    Change babel to babel-loader in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react']
