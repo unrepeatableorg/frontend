358b886b5df8d5ea944b181ea4b588d9fa8a3c04
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-03-01 18:46:16 +0100

    Use stateless function instead of class in Navbar component

--- src/jsx/components/navbar.jsx
+++ src/jsx/components/navbar.jsx
@@ -1,45 +1,38 @@
 import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
-export default class Navbar extends React.Component {
+const Navbar = () => {
 
-  /**
-   * @param {Object} props
-   */
-  constructor(props) {
-    super(props);
+  const linkTypes = [ IndexLink, Link ];
 
-    this.linkTypes = [ IndexLink, Link ];
+  const links = [
+    { title: 'Home', link: '/', type: 0 },
+    { title: 'Keys', link: '/keys', type: 1 },
+    { title: 'About', link: '/about', type: 1 }
+  ];
 
-    this.links = [
-      { title: 'Home', link: '/', type: 0 },
-      { title: 'Keys', link: '/keys', type: 1 },
-      { title: 'About', link: '/about', type: 1 }
-    ];
-  }
-
-  render() {
-    let listItems = this.links.map((item) => {
-      const Element = this.linkTypes[item.type];
-      return (
-        <li key={item.title} className="navbar__list-item">
-          <Element className="navbar__link"
-                   activeClassName="navbar__link--active"
-                   to={item.link}>
-            {item.title}
-          </Element>
-        </li>
-      );
-    });
-
+  const listItems = links.map(item => {
+    const Element = linkTypes[item.type];
     return (
-      <div className="container-fluid navbar">
-        <div className="container">
-          <ul>
-            {listItems}
-          </ul>
-        </div>
-      </div>
+      <li key={item.title} className="navbar__list-item">
+        <Element className="navbar__link"
+                 activeClassName="navbar__link--active"
+                 to={item.link}>
+          {item.title}
+        </Element>
+      </li>
     );
-  };
-}
+  });
+
+  return (
+    <div className="container-fluid navbar">
+      <div className="container">
+        <ul>
+          {listItems}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
