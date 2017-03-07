49010373111a57aa339a25862aa1f48b284746b2
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-03-07 20:34:16 +0100

    Use stateless function instead of class in Main component

--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Navbar from './components/navbar.jsx';
 
-export default class Main extends React.Component {
-  render() {
-    return (
-      <div className="container-fluid">
-        <Navbar />
-        {React.cloneElement(this.props.children, this.props)}
-      </div>
-    )
-  };
-}
+const Main = (props) => (
+  <div className="container-fluid">
+    <Navbar />
+    {React.cloneElement(props.children, props)}
+  </div>
+);
+
+export default Main;
