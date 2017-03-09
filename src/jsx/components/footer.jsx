c6a7604a13769487e199266e8c3d1dd034170ed6
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-03-09 18:25:07 +0100

    Add sticky Footer component

---
+++ src/jsx/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="container-fluid footer">
+    <div className="container">
+      Footer is here!
+    </div>
+  </div>
+);
+
+export default Footer;
--- src/jsx/main.jsx
+++ src/jsx/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from './components/navbar.jsx';
+import Footer from './components/footer.jsx';
 
 const Main = (props) => (
   <div className="container-fluid">
     <Navbar />
     {React.cloneElement(props.children, props)}
+    <Footer />
   </div>
 );
 
