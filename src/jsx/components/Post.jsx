5a7c9c227e68c218055adda15846282f3aaa9a25
Przemyslaw Hardyn <przemyslaw.hardyn+git@gmail.com>
2017-03-16 19:11:04 +0100

    In Post component set props.author as optional and show 'anonymous' if empty

--- src/jsx/components/Post.jsx
+++ src/jsx/components/Post.jsx
@@ -5,7 +5,7 @@ import React from 'react';
  * @params {Object} props.post
  * @params {integer} props.post.id
  * @params {string} props.post.title
- * @params {string} props.post.author
+ * @params {string} [props.post.author]
  * @params {string} [props.post.code]
  * @params {string} [props.post.image]
  */
@@ -34,7 +34,7 @@ const Post = (props) => {
       <div className="post__info">
         <div className="post__title"> {props.post.title} </div>
         <div className="post__creation-info">
-          Added by {props.post.author} on {props.post.date}
+          Added by {props.post.author ? props.post.author : 'anonymous'} on {props.post.date}
         </div>
       </div>
     </div>
